a75acc9de295b001c3566ddc31b6d20364c9f1ce
Diego Flores <dfmonroy07@gmai.com>
2022-09-06 17:45:05 -0500

    feat: added tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        teal: colors.teal,
+        cyan: colors.cyan
+      },
+    },
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+  ],
+}
