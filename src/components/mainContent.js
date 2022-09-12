648ddd57114cbc269b8616583c2284b881a7128c
Diego Flores <dfmonroy07@gmai.com>
2022-09-12 11:39:17 -0500

    feat: adjusted layout content

--- src/components/mainContent.js
+++ src/components/mainContent.js
@@ -109,7 +109,7 @@ const blogPosts = [
 export default function MainContent() {
     return (
         <>
-        <div className="bg-gray-900 pt-10 sm:pt-16 lg:overflow-hidden lg:pt-8 lg:pb-14">
+          <div className="bg-gray-900 pt-10 sm:pt-16 lg:overflow-hidden lg:pt-8 lg:pb-14">
             <div className="mx-auto max-w-7xl lg:px-8">
               <div className="lg:grid lg:grid-cols-2 lg:gap-8">
                 <div className="mx-auto max-w-md px-4 sm:max-w-2xl sm:px-6 sm:text-center lg:flex lg:items-center lg:px-0 lg:text-left">
