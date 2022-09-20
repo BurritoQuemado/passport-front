b5b2da86bb7f6dde5cd0a1118e949f93abb671c4
Diego Flores <dfmonroy07@gmail.com>
2022-09-19 23:05:00 -0500

    feat: fixed communication with backend

--- src/components/scanner.js
+++ src/components/scanner.js
@@ -3,63 +3,68 @@ import QrReader from 'react-qr-scanner';
 import ImgSello from '../media/sello.png'
 
 class Scanner extends Component {
-    constructor(){
-      super();
+    constructor(props){
+      super(props);
       this.state = {
         delay: 1000,
         result: "no data",
+        scanning: true,
         valid_qr: false,
         visited_equipment: "",
-        valid_scans: []
+        valid_scans: [],
+        user_id: this.props.user_id
       }
     }
 
     componentDidMount(){
-      fetch('http://localhost:3000/equipment')
+      fetch('http://localhost:3000/equipments')
       .then(response => response.json())
       .then(equipment => {
-        console.log(equipment)
         this.setState({valid_scans: equipment})
-      })
-      .then(console.log(this.state.valid_scans));
+      });
     }
 
-    
+    setEquipmentName = (name, send) => {
+      this.setState({visited_equipment: name}, () => {
+        this.sendData(send);
+      });
+    }
+
+    setValidQR = (found) => {
+      this.setState({valid_qr: found})
+    } 
+
     validateQR = (text) => {
-      let found = false;
       this.state.valid_scans.forEach(valid => {
-        if(valid.id === text){
-          found = true;
-          this.setState({
-            visited_equipment: valid.name
-          });
+        if(valid.code === text){
+          this.setValidQR(true);
+          this.setEquipmentName(valid.name, true);
         }
       });
-      if(found) {
-        this.sendData();
-      }
-      return found;
     } 
     
-    sendData = () => {
-      fetch('http://localhost:3000/attend', {
-        method: 'post',
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify({
-          id: '124',
-          equipment: this.state.visited_equipment
+    sendData = (send) => {
+      if(send) {
+        fetch('http://localhost:3000/attend', {
+          method: 'post',
+          headers: {'Content-Type': 'application/json'},
+          body: JSON.stringify({
+            user_id: this.state.user_id,
+            equipment_name: this.state.visited_equipment
+          })
         })
-      })
-      .then(response => response.json())
-      .then(console.log)
+        .then(response => response.json())
+        .then(console.log)
+      }
     }
 
     handleScan = (data) => {
       if(data != null){
         this.setState({
+          scanning: false,
           result: data.text,
-          valid_qr: this.validateQR(data.text)
         });
+        this.validateQR(data.text);
       }
     };
 
@@ -70,7 +75,9 @@ class Scanner extends Component {
     cleanScan = () => {
       this.setState({
         result: "no data",
-        valid_qr: false
+        valid_qr: false,
+        scanning: true,
+        visited_equipment: "",
       });
     }
 
@@ -81,11 +88,16 @@ class Scanner extends Component {
             <h2 className="text-3xl font-bold tracking-tight text-morado_abalat sm:text-4xl uppercase">Escanea el código QR</h2>
           </div>
           <div className='pt-6 pb-6 pr-4 pl-4 bg-gray-200'>
+            { this.state.scanning?
             <QrReader
-              delay={this.state.delay}
-              onError={this.handleError}
-              onScan={this.handleScan}
-            />
+            delay={this.state.delay}
+            onError={this.handleError}
+            onScan={this.handleScan}
+          /> : 
+          <div className='justify-center mt-6'>
+            <h1 className='text-center'>Escaneando</h1>
+          </div>
+          }  
           </div>
           {
             this.state.valid_qr ? 
@@ -96,7 +108,7 @@ class Scanner extends Component {
                     src={ ImgSello }
                     alt="Abalat"
                 />
-              <div className="px-4 py-3 text-center sm:px-6">
+                <div className="px-4 py-3 text-center sm:px-6">
                     <button
                         onClick={this.cleanScan}
                         className="inline-flex justify-center rounded-md border border-transparent bg-azul_abalat py-2 px-4 text-sm lg:text-2xl lg:font-semibold font-medium text-white shadow-sm focus:outline-none focus:ring-2 focus:ring-azul_abalat focus:ring-offset-2"
--- src/pages/escanear.js
+++ src/pages/escanear.js
@@ -1,9 +1,27 @@
+import React, { Component } from 'react';
 import Scanner from "../components/scanner";
 
-export default function Escanear() {
-    return (
-        <>
-            <Scanner />
-        </>
-    );
+export default class Escanear extends Component {
+    constructor(){
+        super();
+        this.state = {
+            user_id: '7'
+        }
+    }
+
+    componentDidMount(){
+        fetch('http://localhost:3000/equipments')
+        .then(resp => resp.json())
+        .then(equips => {
+            this.setState({equipment_list: equips})
+        })
+    }
+
+    render(){
+        return (
+            <>
+                <Scanner user_id={this.state.user_id}/>
+            </>
+        );
+    }
 }
