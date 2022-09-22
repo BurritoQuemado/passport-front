7b2a181bd0480d089a66c6cd0baf077fb4205026
= <=>
2022-09-22 15:13:39 -0500

    feat: fixed request bug

--- src/pages/equipments.js
+++ src/pages/equipments.js
@@ -6,17 +6,19 @@ export default class Equipments extends Component {
     constructor(props){
         super(props);
         this.state = {
-            user_id: this.props.user_id,
+            user_id: Number(this.props.user_id),
             user_attendee: []
         }
     }
 
     componentDidMount(){
-        fetch(`https://pure-atoll-64455.herokuapp.com/attendance_list/${encodeURIComponent(Number(this.state.user_id))}`)
+        fetch(`https://pure-atoll-64455.herokuapp.com/attendance_list/${encodeURIComponent(this.state.user_id)}`)
         .then(response => response.json())
         .then(equipment => {
             this.setState({user_attendee: equipment})
         });
+
+        console.log(this.state.user_id);
     }
 
     render(){
